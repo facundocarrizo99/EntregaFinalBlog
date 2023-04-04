911904f34b9712939561c873853bbe57b58e36a3
facundocarrizo99 <49492465+facundocarrizo99@users.noreply.github.com>
2023-04-04 18:49:57 -0300

    Primeras bases de Posts

--- PostsApp/models.py
+++ PostsApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=128)
+    subTitulo = models.CharField(max_length=256)
+    cuerpo = models.CharField(max_length=1024)
+    autor = models.ForeignKey()#To Do ver como linkea con un blogger en la BD
+    fecha = models.DateTimeField
+    foto = models.FileField
+
