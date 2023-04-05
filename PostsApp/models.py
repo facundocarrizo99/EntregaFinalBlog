f6ff596bccbe6d2314a2f4de6f559827f3276878
facundocarrizo99 <49492465+facundocarrizo99@users.noreply.github.com>
2023-04-05 12:12:49 -0300

    Cambio de Consigna, Messages no va mas, se piden comentarios, no chat

--- PostsApp/models.py
+++ PostsApp/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from LogInApp.models import Blogger
 
 # Create your models here.
 class Post(models.Model):
     titulo = models.CharField(max_length=128)
     subTitulo = models.CharField(max_length=256)
     cuerpo = models.CharField(max_length=1024)
-    autor = models.ForeignKey()#To Do ver como linkea con un blogger en la BD
+    owner = models.ForeignKey(Blogger, on_delete=models.CASCADE)
     fecha = models.DateTimeField
     foto = models.FileField
 
+
+class Comment(models.Model):
+    commentOwner = models.ForeignKey(Blogger, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=150)
+    fecha = models.DateTimeField
